f30a013494f1d3a9df00116d3baf2ab2a17d453e
seemaekal5 <46122853+seemaekal5@users.noreply.github.com>
2018-12-25 12:39:24 +0530

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'seema_ru_ru',
+    __DIR__
+);
